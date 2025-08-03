4ae091f533d80758b8d1444704b96f79996d7b3a
taKana671 <kanae5321@gmail.com>
2025-08-03 16:41:53 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,55 @@
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+
+# extensions = [
+#     Extension(
+#         '*',
+#         ['noise/cynoise/*.pyx', 'noise/cynoise/voronoi/*.pyx'],
+#         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
+#     )
+# ]
+
+# setup(
+#     ext_modules=cythonize(
+#         extensions,
+#         compiler_directives={'profile': False}
+#     )
+# )
+
+
+
+
+import glob
+import pathlib
+
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+
+def create_ext():
+    for p in glob.iglob('noise/cynoise/**/*pyx', recursive=True):
+        path = pathlib.Path(p)
+
+        name = path.name
+        posix_path = path.as_posix()
+        parents = posix_path.split('/')[:-1]
+
+        yield Extension(
+            f'{".".join(parents)}.{name.split(".")[0]}',
+            [f'{"/".join(parents)}/{name}'],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
+        )
+
+
+extensions = [ext for ext in create_ext()]
+
+setup(
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            'profile': False,
+            'language_level': "3"
+        }
+    )
+)
