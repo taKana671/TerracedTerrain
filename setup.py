9d713cf4ca1a98c17d66dd3fc695d8e5db770590
taKana671 <kanae5321@gmail.com>
2025-08-11 14:56:08 +0900

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-
-
-# extensions = [
-#     Extension(
-#         '*',
-#         ['noise/cynoise/*.pyx', 'noise/cynoise/voronoi/*.pyx'],
-#         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
-#     )
-# ]
-
-# setup(
-#     ext_modules=cythonize(
-#         extensions,
-#         compiler_directives={'profile': False}
-#     )
-# )
-
-
-
-
 import glob
 import pathlib
 
